82629892c47c3daa171a7f6f9839b75713c97ad7
MikuLink <k19831221@163.com>
2021-09-19 15:20:55 +0800

    everywhere指令父类添加权限校验方法

--- src/main/java/cn/mikulink/rabbitbot/command/everywhere/BaseEveryWhereCommand.java
+++ src/main/java/cn/mikulink/rabbitbot/command/everywhere/BaseEveryWhereCommand.java
@@ -2,6 +2,12 @@ package cn.mikulink.rabbitbot.command.everywhere;
 
 
 import cn.mikulink.rabbitbot.command.EverywhereCommand;
+import net.mamoe.mirai.contact.Contact;
+import net.mamoe.mirai.contact.User;
+import net.mamoe.mirai.message.data.Message;
+import net.mamoe.mirai.message.data.MessageChain;
+
+import java.util.ArrayList;
 
 /**
  * @author MikuLink
@@ -12,5 +18,16 @@ import cn.mikulink.rabbitbot.command.EverywhereCommand;
  */
 public abstract class BaseEveryWhereCommand implements EverywhereCommand {
 
-
+    /**
+     * 权限验证
+     *
+     * @param sender       消息发送人
+     * @param args         指令追加参数
+     * @param messageChain 消息对象 第一个元素一定为 [MessageSource], 存储此消息的发送人, 发送时间, 收信人, 消息 id 等数据. 随后的元素为拥有顺序的真实消息内容.
+     * @param subject      消息主体
+     * @return 权限验证结果，返回null表示不做校验
+     */
+    public Message permissionCheck(User sender, ArrayList<String> args, MessageChain messageChain, Contact subject) {
+        return null;
+    }
 }
